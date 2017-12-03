ac472a72a9f7843ca571877755f72835ba7c3321
vp2020 <33272034+vp2020@users.noreply.github.com>
2017-12-03 18:26:33 -0500

    Add files via upload

---
+++ dailyTemperature.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+ * Created by Viral on 12/3/2017.
+ * Leetcode: 739. Daily Temperatures
+ */
+public class dailyTemperature {
+    public static void main(String args[]) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        for(int x : findDailyTemperature(temperatures)) {
+            System.out.println(x);
+        }
+    }
+
+    private static int[] findDailyTemperature(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[temperatures.length];
+        int a[] = temperatures;
+        for(int index = a.length-1; index >= 0; index--) {
+            while(!stack.isEmpty() && a[stack.peek()] <= a[index]) {
+                stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                res[index] = stack.peek() - index;
+            } else {
+                res[index] = 0;
+            }
+            stack.push(index);
+        }
+        return res;
+
+    }
+}
